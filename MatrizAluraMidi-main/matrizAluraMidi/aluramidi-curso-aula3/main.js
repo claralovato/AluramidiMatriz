79b5cd3f1b7501c8eac31e1853ff0c40b7e6769d
claralovato <111514087+claralovato@users.noreply.github.com>
2023-10-31 09:57:27 -0300

    Add files via upload

---
+++ MatrizAluraMidi-main/matrizAluraMidi/aluramidi-curso-aula3/main.js
@@ -0,0 +1,29 @@
+
+
+
+
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+
+//O que é uma constante...
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+
+
+
+let contador= 0; 
+
+while (contador < listaDeTeclas.length) {
+   const efeito =
+
+   listaDeTeclas[contador].onclick = tocaSomPom
+   contador = contador + 1;
+   console.leg(contador);
+}
+
+
+document.querySelector('.tecla_pom');
